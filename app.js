876b13ea8fe80b86b6b191dddf149b061384d13e
brettc84 <brett.coomer@gmail.com>
2016-09-25 21:08:43 +0200

    simple bot intents

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ var builder = require('botbuilder');
 
 var connector = new builder.ConsoleConnector().listen();
 var bot = new builder.UniversalBot(connector);
-bot.dialog('/', function (session) {
-    session.send('Hello World');
-});
+var model = 'https://api.projectoxford.ai/luis/v1/application?id=c413b2ef-382c-45bd-8ff0-f76d60e2a821&subscription-key=2e6d421c24804da38180f597a4a69a8e&q='
+
+
+var recognizer = new builder.LuisRecognizer(model);
+var dialog = new builder.IntentDialog({ recognizers: [recognizer] });
+bot.dialog('/', dialog);
+
+// Add intent handlers
+dialog.matches('builtin.intent.alarm.set_alarm', builder.DialogAction.send('Creating Alarm'));
+dialog.matches('builtin.intent.alarm.delete_alarm', builder.DialogAction.send('Deleting Alarm'));
+dialog.onDefault(builder.DialogAction.send("I'm sorry I didn't understand. I can only create & delete alarms."));
